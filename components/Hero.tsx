aca4dae7f0bcea291b17b6408c573104418b63e8
boluwatifeajayi <bolu4good@gmail.com>
2023-09-17 16:56:31 +0100

    updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 'use client'
-import React from 'react';
+
+import React, {useEffect, useState} from 'react';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import apple from '../assets/images/apple.png';
@@ -7,7 +8,8 @@ import Link from 'next/link';
 import play from '../assets/images/play.png';
 import country from '../assets/images/country.png';
 import phone from '../assets/images/phone-hero.svg';
-import bg from '../assets/images/bg-plane.png'
+import bg from '../assets/images/bg-plane.png';
+import scroll from '../assets/images/scroll.png'
 
 const Hero = () => {
   const gradientBackground = {
@@ -16,23 +18,45 @@ const Hero = () => {
 
   const phoneVariants = {
     hidden: {
-      y: 200
+      y: 200,
     },
     visible: {
       y: 0,
       transition: {
-        duration: 0.5
+        duration: 0.5,
+      },
+    },
+  };
+
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
       }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
   return (
-   <div style={{ position: 'relative' }}>
-      <Image
-        src={bg}
-        alt="Background"
-        style={{ width: '100%', height: '100vh' }}
-      />
+    <div style={{ position: 'relative' }}>
+      <Image src={bg} alt="Background" style={{ width: '100%', height: '100vh' }} />
       <section
         id="hero"
         className="px-5 main-hero hero-2 md:px-3"
@@ -44,29 +68,49 @@ const Hero = () => {
           bottom: 0,
         }}
       >
-        <div className="block md:flex main-hero mt-2 md:mt-60 pb-52 main-hero md:pb-0 pt-32 md:pt-24 md:gap-44 hero">
+        <div className="block md:flex main-hero mt-2 md:mt-48 pb-52 main-hero md:pb-0 pt-32 md:pt-24 md:gap-44 hero">
           <div className="flex flex-col">
             <div>
               <h2 className="text-5xl font-bold text-blue-50">
                 We offer the <br /> ultimate experience <br /> in luxury travel
               </h2>
             </div>
-            
+
             <Image className="ml-0 md:ml-0 mt-6" src={country} alt="countries" />
           </div>
           <div>
-          <p className="text-white text-lg mt-6">
-            Immerse yourself in the ultimate luxury travel experience with<br /> us. Our world of opulence is tailored to elevate every moment <br /> of your journey.
-          </p>
-          <div className='flex mt-4 md:mt-auto'>
-            <Link href="#">
-              <Image className='' src={play} alt='playstore' />
-            </Link>
-            <Link href="#">
-              <Image className='' src={apple} alt='appstore' />
-            </Link>
+            <p className="text-white text-lg mt-6">
+              Immerse yourself in the ultimate luxury travel experience with<br /> us. Our world of opulence is tailored to elevate every moment <br /> of your journey.
+            </p>
+            <div className="flex mt-4 md:mt-auto">
+              <Link href="#">
+                <Image className="" src={play} alt="playstore" />
+              </Link>
+              <Link href="#">
+                <Image className="" src={apple} alt="appstore" />
+              </Link>
             </div>
           </div>
+        </div>
+        <div className="text-center mt-20">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, y: 10 }}
+            transition={{
+              repeat: Infinity,
+              repeatType: 'reverse',
+              duration: 1,
+              delay: 0.5,
+            }}
+          >
+
+              <span onClick={() => scrollToSection('about-us')} className='cursor-pointer'>
+              <Image className="h-full w-6 text-white mx-auto" src={scroll} alt="appstore" />
+              <p className='text-xs mt-3 text-white'>CLICK TO SCROLL</p>
+              </span>
+              
+            
+          </motion.div>
         </div>
       </section>
     </div>
--- components/Requst.tsx
+++ components/Requst.tsx
@@ -1,109 +1,114 @@
 'use client'
 import React, { useState } from 'react';
 import Image from 'next/image';
-import tick from '../assets/images/ticket.png'
-
+import tick from '../assets/images/ticket.png';
 
 const Request: React.FC = () => {
-  
-
   return (
-    <section id="request-quote" className='story py-8 bg-[#F0F9FF]'>
-      <div className='block md:flex'>
-        <div className='w-full md:w-1/2 hero'>
+    <section id="request-quote" className="story py-8 bg-[#F0F9FF]">
+      <div className="block md:flex">
+        <div className="w-full md:w-1/2 hero">
           <Image src={tick} alt="image" />
         </div>
-        <div className='bg-[#F0F9FF] w-full md:w-1/2'>
-          <div className='mt-16 hero mx-6 md:mx-14'>
-          <p className='text-[#0129A4] font-bold'>REQUEST QUOTE</p>
-          <p className='text-[#18254A] text-4xl font-bold mt-2 mb-5'>
-            We leave the customising<br/> to you.
-          </p>
+        <div className="bg-[#F0F9FF] w-full md:w-1/2">
+          <div className="mt-16 hero mx-6 md:mx-14">
+            <p className="text-[#0129A4] font-bold">REQUEST QUOTE</p>
+            <p className="text-[#18254A] text-4xl font-bold mt-2 mb-5">We leave the customising<br /> to you.</p>
 
-          <form className='mt-10'>
+            <form className="mt-10">
+              <div className="flex gap-5">
+                <div className="flex-1">
+                  <label className="text-[#18254A] mb-3">Full Name</label>
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    className="w-full px-2 mt-2 py-2 border-[#d4d6dc] bg-transparent border rounded"
+                    placeholder="Enter Your Full name"
+                    required
+                  />
+                </div>
+                <div className="flex-1">
+                  <label className="text-[#18254A] mb-3">Email Address</label>
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    className="w-full mt-2 px-2 py-2 border-[#d6d6d8] bg-transparent border rounded"
+                    placeholder="Enter Your Email address"
+                    required
+                  />
+                </div>
+              </div>
 
-            <div className='flex gap-5'>
-            <div>
-            <label className='text-[#18254A] mb-3'>Full Name</label>
-            <input
-                type="text"
-                id="email"
-                name="email"
-                className="w-full px-2 mt-2 py-2 border-[#d4d6dc] bg-transparent border rounded"
-                placeholder='Enter Your Full name'
-                required
-            />
-            </div>
-            <div>
-            <label className='text-[#18254A] mb-3'>Email Address</label>
-            <input
-                type="text"
-                id="email"
-                name="email"
-                className="w-full mt-2 px-2 py-2 border-[#d6d6d8] bg-transparent border rounded"
-                placeholder='Enter Your Email address'
-                required
-            />
-            </div>
-           
-            </div>
-            <div className='flex mt-7 gap-5'>
-            <div>
-            <label className='text-[#18254A] mb-3'>Take Off</label>
-            <input
-                type="text"
-                id="email"
-                name="email"
-                className="w-full px-2 mt-2 py-2 border-[#d4d6dc] bg-transparent border rounded"
-                placeholder='Enter Take Off Location'
-                required
-            />
-            </div>
-            <div>
-            <label className='text-[#18254A] mb-3'>Destination</label>
-            <input
-                type="text"
-                id="email"
-                name="email"
-                className="w-full mt-2 px-2 py-2 border-[#d6d6d8] bg-transparent border rounded"
-                placeholder='Where Are You heading Too'
-                required
-            />
-            </div>
-           
-            </div>
+              <div className="flex mt-7 gap-5">
+                <div className="flex-1">
+                  <label className="text-[#18254A] mb-3">Take Off</label>
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    className="w-full px-2 mt-2 py-2 border-[#d4d6dc] bg-transparent border rounded"
+                    placeholder="Enter Take Off Location"
+                    required
+                  />
+                </div>
+                <div className="flex-1">
+                  <label className="text-[#18254A] mb-3">Destination</label>
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    className="w-full mt-2 px-2 py-2 border-[#d6d6d8] bg-transparent border rounded"
+                    placeholder="Where Are You Heading To"
+                    required
+                  />
+                </div>
+              </div>
 
-            <div className='flex mt-7 gap-4'>
-            <div className='w-1/2' >
-            <label className='text-[#18254A] mb-3'>Choose Date</label>
-            <input
-                type="text"
-                id="email"
-                name="email"
-                className="w-full px-2 mt-2 py-2 border-[#d4d6dc] bg-transparent border rounded"
-                placeholder='Choose Date'
-                required
-            />
-            </div>
-            <div className='w-1/2'>
-            <label className='text-[#18254A] mb-3'>Jet Type</label>
-            <input
-                type="text"
-                id="email"
-                name="email"
-                className="w-full mt-2 px-2 py-2 border-[#d6d6d8] bg-transparent border rounded"
-                placeholder='Select Jet Type'
-                required
-            />
-            </div>
-           
-            </div>
-            <button className="text-white px-5 py-3 text-sm font-semibold bg-[#0129A4] w-full mt-10 rounded">Request Quote</button>
-          </form>
+              <div className="flex mt-7 gap-5">
+                <div className="flex-1">
+                  <label className="text-[#18254A] mb-3">Type Of Trip</label>
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    className="w-full px-2 mt-2 py-2 border-[#d4d6dc] bg-transparent border rounded"
+                    placeholder="Select Type Of Trip"
+                    required
+                  />
+                </div>
+                <div className="flex-1">
+                  <label className="text-[#18254A] mb-3">Date</label>
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    className="w-full mt-2 px-2 py-2 border-[#d6d6d8] bg-transparent border rounded"
+                    placeholder="Select Date"
+                    required
+                  />
+                </div>
+              </div>
 
-        </div>
-
-         
+              <div className="flex mt-7 gap-5">
+                <div className="flex-1 w-1/2">
+                  <label className="text-[#18254A] mb-3">Jet Type</label>
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    className="w-full px-2 mt-2 py-2 border-[#d4d6dc] bg-transparent border rounded"
+                    placeholder="Select Type Of Jet"
+                    required
+                  />
+                </div>
+              </div>
+              <button className="text-white px-5 py-3 text-sm font-semibold bg-[#0129A4] w-full mt-10 rounded">
+                Request Quote
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </section>
